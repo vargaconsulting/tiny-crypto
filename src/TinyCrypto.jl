b3644f7a93fa95f9d25dfa793bc29dae77985b74
svarga <steven@vargaconsulting.ca>
2025-05-23 15:25:22 -0400

    [#19]:svarga:feature, adding 8 and 16 bit tiny hash

--- src/TinyCrypto.jl
+++ src/TinyCrypto.jl
@@ -3,16 +3,19 @@ module TinyCrypto
 include("macroes.jl")
 include("utils.jl")
 include("field.jl")
+include("core/hash.jl")
 
 using .Macroes
 using .Field
 using .Utils
+using .Hash
 
 include("core/ecpoint.jl")
 include("curves/weierstrass.jl")
 
     # Re-export core symbols for public API
     export @define, @attach
+    export H, H₈, H₁₆, H8, H16
     export Fp, 𝔽ₚ
     export is_prime, primes, mod_inverse
     export Point, ∞, Curve, ECPoint, Infinity
