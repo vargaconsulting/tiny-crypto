171c1346e03f56061bb06ec677c7ebe9c7b8ca0e
svarga <steven@vargaconsulting.ca>
2025-07-10 21:55:59 -0400

    [#28]:svarga:ecdsa, add ecrecover to reconstruct public key from signature

--- src/core/ecdsa.jl
+++ src/core/ecdsa.jl
@@ -39,4 +39,33 @@ module ECDSA
         P = u₁ * curve.G + u₂ * pub
         return mod(P.point.x, curve.order) == r
     end
+    function ecrecover(curve::Weierstrass, msg::AbstractString, r::Int, s::Int, v::Int)
+        n, π = curve.order, curve.π
+        z = Int(H₈(msg)) % n
+        x_candidates = [r + k*n for k in 0:div(π,n) if r + k*n < π]
+    
+        for x in x_candidates
+            α = mod(x^3 + curve.a * x + curve.b, π)
+            y_candidates = [y for y in 0:π-1 if y^2 % π == α]
+            for y in y_candidates
+                if isodd(y) == v
+                    R = ECPoint(x, y, curve)
+                    r_inv = invmod(r, n)
+                    sR = s * R
+                    zG = z * curve.G
+                    neg_zG = ECPoint(zG.point.x, -zG.point.y, curve)  # handle point negation properly
+                    pub = r_inv * (sR + neg_zG)
+                    return pub
+                end
+            end
+        end
+        return nothing
+    end
+    function ecrecover(curve::Weierstrass, msg::AbstractString, signature::NTuple{3,Int})
+        r,s,v = signature
+        return ecrecover(curve, msg, r,s,v)
+    end
+    function ecrecover(curve::Weierstrass, msg::AbstractString, signature::NamedTuple{(:r, :s, :v), <:Tuple{<:Integer, <:Integer, <:Integer}})
+        return ecrecover(curve, msg, signature.r, signature.s, signature.v)
+    end
 end
